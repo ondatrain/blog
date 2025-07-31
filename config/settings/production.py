36676c8bffbe65bbb9a0f8357eb01c95763edb6f
ondatrain <on@datrain.dev>
2025-07-31 19:37:46 -0400

    Base de datos de producción configurada

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,17 +1,80 @@
 from .base import *
+import environ
+import os
 
-DEBUG = False
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
+if os.environ.get('REMOTE_HOSTED_BLOG') is None:
+    # Take environment variables from .env file
+    environ.Env.read_env(os.path.join(BASE_DIR, '.env.prod'))
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+# False if not in os.environ because of casting above
+DEBUG = env('DEBUG')
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
+
+db_params = {
+    'default': env.db(),
 }
+
+# Secure connection using a public certificate
+CERT_DIR = env('CERT_DIR')
+SSL_ROOT_CERT = os.path.join(BASE_DIR, CERT_DIR)
+
+db_params['default'].update({
+    'OPTIONS': {
+        'sslmode': 'verify-ca',
+        'sslrootcert': SSL_ROOT_CERT
+    }
+})
+
+DATABASES = db_params
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', cast=str)
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+if env.bool('REMOTE_HOSTED_BLOG', default=False) and env.bool('LOG_ENABLED', default=False):
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'verbose': {
+                'format': '{levelname} {asctime} {module} {message}',
+                'style': '{',
+            },
+            'simple': {
+                'format': '{levelname} {message}',
+                'style': '{',
+            },
+        },
+        'handlers': {
+            'file': {
+                'level': 'DEBUG',
+                'class': 'logging.FileHandler',
+                'filename': 'debug.log',
+                'formatter': 'verbose',
+            },
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': env('LOG_CONSOLE_LEVEL'),
+            },
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+        },
+    }
 
 
 try:
--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import environ
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+blog_control = environ.Env(
+    # set casting, default value
+    BLOG_SETTINGS_FILE=(str, "config.settings.dev")
+)
+
+blog_control_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+environ.Env.read_env(os.path.join(blog_control_dir, '.blog_control'))
+
+BLOG_SETTINGS_FILE = blog_control('SETTINGS_FILE')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", BLOG_SETTINGS_FILE)
 
 application = get_wsgi_application()
